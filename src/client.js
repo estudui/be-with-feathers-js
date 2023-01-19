46fbca05152c90972cbcf04133d80ff74803f096
estu <estudwiyanto21@gmail.com>
2023-01-19 18:22:11 +0700

    initial framework 🎉🎉

---
+++ src/client.js
@@ -0,0 +1,25 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/client.html
+import { feathers } from '@feathersjs/feathers'
+import authenticationClient from '@feathersjs/authentication-client'
+export const userServiceMethods = ['find', 'get', 'create', 'patch', 'remove']
+
+/**
+ * Returns a  client for the be-with-feathers-js app.
+ *
+ * @param connection The REST or Socket.io Feathers client connection
+ * @param authenticationOptions Additional settings for the authentication client
+ * @see https://dove.feathersjs.com/api/client.html
+ * @returns The Feathers client application
+ */
+export const createClient = (connection, authenticationOptions = {}) => {
+  const client = feathers()
+
+  client.configure(connection)
+  client.configure(authenticationClient(authenticationOptions))
+
+  client.use('users', connection.service('users'), {
+    methods: userServiceMethods
+  })
+
+  return client
+}
